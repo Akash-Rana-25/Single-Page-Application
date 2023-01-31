929e784db450bd9b19944d087ba0de02e20b1672
Akash-Rana-25 <akashrana.work.gmail.com>
2023-01-31 14:30:31 +0530

    added

---
+++ image.js
@@ -0,0 +1,27 @@
+let slide = 1;
+showSlides(slide);
+
+
+function next(n) {
+  showSlides(slide += n);
+}
+
+function display(n) {
+  showSlides(slide = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slide = 1}
+  if (n < 1) {slide = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+ 
+  slides[slide-1].style.display = "block";
+}
+
+function myFunction(content) {
+ document.getElementById(content).scrollIntoView(); 
+}
