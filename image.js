dbb90af53bb6d9395fa81922157b4002ac1cbff1
Akash-Rana-25 <akashrana.work.gmail.com>
2023-02-06 10:20:25 +0530

    Improvement on Images section. When clicking next button the page scrolls up or down automatically. Please improve that

--- image.js
+++ image.js
@@ -23,5 +23,5 @@ function showSlides(n) {
 }
 
 function myFunction(content) {
- document.getElementById(content).scrollIntoView({behavior: "smooth"}); 
+ document.getElementById(content).scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"}); 
 }
